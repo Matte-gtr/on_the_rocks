e6efe5180c47a811fa74a57e22a8df8b592e38ce
Matt <matt.snell.00@hotmail.co.uk>
2020-10-21 16:58:31 +0000

    add styling and display functionality to create a crate page

--- crate/views.py
+++ crate/views.py
@@ -1,9 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from products.models import Product, Category
 
 
 def create_a_crate(request):
     template = "crate/create_a_crate.html"
-    context = {
+    all_categories = Category.objects.all()
+    category_selected = False
+    products = Product.objects.all()
+    categories = ''
 
+    if request.method == 'GET':
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+            category_selected = request.GET['category']
+
+    context = {
+        'all_categories': all_categories,
+        'page_header': 'Create-a-crate',
+        'categories': categories,
+        'products': products,
+        'category_selected': category_selected,
     }
     return render(request, template, context)
+
+
+def add_to_crate(request, product_id):
+    """ add a product to the crate """
+    crate = request.session.get('crate', {})
+    quantity = 1
+
+    if product_id in list(crate.keys()):
+        crate[product_id] += quantity
+    else:
+        crate[product_id] = quantity
+
+    request.session['crate'] = crate
+    return redirect(reverse('create_a_crate'))
