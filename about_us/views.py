e51facc7a72579d6f335a37b16fa06b604e9859d
Matt <matt.snell.00@hotmail.co.uk>
2020-10-30 01:34:37 +0000

    add more styling to about us page and adjust view

--- about_us/views.py
+++ about_us/views.py
@@ -6,18 +6,21 @@ from checkout.models import OrderLineItem
 
 def about_us(request):
     """ a view to display the about us page """
+    first_product = get_object_or_404(Product, pk=27)
     product_ids = OrderLineItem.objects.values_list('product').\
-        annotate(product_count=Count('product')).order_by('product')[:5]
-    if len(product_ids) < 5:
-        popular_products = Product.objects.all()[:5]
+        annotate(product_count=Count('product')).order_by('product')[:6]
+    if len(product_ids) < 6:
+        popular_products = Product.objects.all()[:6]
     else:
         popular_products = []
         for key, value in product_ids:
-            popular_products.append(Product, pk=key)
+            item = get_object_or_404(Product, pk=key)
+            popular_products.append(item)
 
     template = 'about_us/about_us.html'
     context = {
         'page_header': 'About us',
         'products': popular_products,
+        'first_product': first_product,
     }
     return render(request, template, context)
