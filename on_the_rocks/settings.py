1fc48d49bc2a5e524e3d18938a2b5b18d2a95aef
Matt <matt.snell.00@hotmail.co.uk>
2020-10-30 23:52:11 +0000

    add cache control to settings

--- on_the_rocks/settings.py
+++ on_the_rocks/settings.py
@@ -182,6 +182,11 @@ FREE_SHIPPING_THRESHOLD = 50
 STANDARD_SHIPPING_COST = 6
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000'
+    }
     # Bucket config
     AWS_STORAGE_BUCKET_NAME = 'msnell89-on-the-rocks'
     AWS_S3_REGION_NAME = 'eu-west-1'
