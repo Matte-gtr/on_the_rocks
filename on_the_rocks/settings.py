4e5ec233f2df4ac27abe84dcceae10cd1bbc0fac
Matt <matt.snell.00@hotmail.co.uk>
2020-10-30 19:24:56 +0000

    add new secret key in environment with link in settings

--- on_the_rocks/settings.py
+++ on_the_rocks/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5zfuwi&n!d=)mte+z$9g$tpdswfr25@_3so4r%1o1f3vyr-e&8'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['msnell89-on-the-rocks.herokuapp.com', 'localhost']
 
