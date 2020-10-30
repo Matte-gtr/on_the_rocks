5a5f9d1febf73d08bb2b6d0dc23c70fb02282135
Matt <matt.snell.00@hotmail.co.uk>
2020-10-30 02:48:51 +0000

    fix bug in about us view

--- about_us/views.py
+++ about_us/views.py
@@ -6,16 +6,21 @@ from checkout.models import OrderLineItem
 
 def about_us(request):
     """ a view to display the about us page """
-    first_product = get_object_or_404(Product, pk=27)
+    first_product_id = OrderLineItem.objects.values_list('product').\
+        annotate(product_count=Count('product')).order_by('product')[:1]
     product_ids = OrderLineItem.objects.values_list('product').\
-        annotate(product_count=Count('product')).order_by('product')[:6]
-    if len(product_ids) < 6:
-        popular_products = Product.objects.all()[:6]
+        annotate(product_count=Count('product')).order_by('product')[1:6]
+
+    if len(product_ids) < 3:
+        popular_products = Product.objects.all()[1:6]
+        first_product = Product.objects.all()[:1][0]
     else:
         popular_products = []
         for key, value in product_ids:
             item = get_object_or_404(Product, pk=key)
             popular_products.append(item)
+            first_product = get_object_or_404(Product,
+                                              pk=first_product_id[0][0])
 
     template = 'about_us/about_us.html'
     context = {
