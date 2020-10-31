4764f85d4da7a025cdf0e66f47dce623a1a61c95
Matt <matt.snell.00@hotmail.co.uk>
2020-10-31 00:13:45 +0000

    update settings.py with correct stripe webhook variable

--- on_the_rocks/settings.py
+++ on_the_rocks/settings.py
@@ -207,5 +207,5 @@ if 'USE_AWS' in os.environ:
 STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
-STRIPE_WEBHOOK_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+STRIPE_WEBHOOK_SECRET = os.getenv('STRIPE_WEBHOOK_SECRET', '')
 DEFAULT_FROM_EMAIL = 'matt.snell.00@hotmail.co.uk'
